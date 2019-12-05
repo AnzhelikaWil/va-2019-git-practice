e71f0047c342923474691560c209ef3423ca324c
AnzhelikaWil <55207820+AnzhelikaWil@users.noreply.github.com>
2019-12-05 15:45:32 -0500

    Update SecondClass.java

--- src/main/java/SecondClass.java
+++ src/main/java/SecondClass.java
@@ -2,6 +2,6 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("second class");
         System.out.println("2nd class");
-          System.out.println("3nd class");
+          System.out.println("Luis b-day");
     }
 }
